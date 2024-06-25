66808533fc447ddc86f67c919105173232b9bd2e
Jake Darby <designdarby@gmail.com>
2024-06-25 13:33:03 +0100

    CSS styling workshop

---
+++ week06/react-css-styling/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header className="my-header">
+      <h1>My App</h1>
+    </header>
+  );
+}
---
+++ week06/react-css-styling/src/components/Section.jsx
@@ -0,0 +1,12 @@
+export default function Section() {
+  return (
+    <>
+      <h1>Cute dog 1</h1>
+      <h2>He wants a treat 🦴</h2>
+      <img
+        src="https://images.unsplash.com/photo-1518717758536-85ae29035b6d?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="cute brown dog with toungue out"
+      />
+    </>
+  );
+}
